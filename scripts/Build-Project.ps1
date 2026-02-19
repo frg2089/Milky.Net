14e6161aae421435ac6f184d431e951a8e4d2c8c
舰队的偶像-岛风酱! <frg2089@outlook.com>
2026-02-20 01:13:39 +0800

    fix: 添加错误处理以在脚本失败时抛出异常
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

--- scripts/Build-Project.ps1
+++ scripts/Build-Project.ps1
@@ -5,17 +5,28 @@ param (
     $Configuration = 'Release'
 )
 
+$ErrorActionPreference = 'Stop'
+
 $Private:ProjectRoot = Join-Path $PSScriptRoot '..'
 $Private:ArtifactsPath = Join-Path $Private:ProjectRoot 'artifacts'
 
 Write-Host 'Restoring...' -ForegroundColor Blue
 dotnet restore --graph --artifacts-path $Private:ArtifactsPath
+if ($LASTEXITCODE -ne 0) {
+  throw 'Restore failed.'
+}
 
 Write-Host 'Generating...' -ForegroundColor Blue
 . $Private:ProjectRoot/scripts/Init-Project.ps1
 
 Write-Host 'Building...' -ForegroundColor Blue
 dotnet build --graph --artifacts-path $Private:ArtifactsPath --configuration $Configuration --no-restore
+if ($LASTEXITCODE -ne 0) {
+  throw 'Build failed.'
+}
 
 Write-Host 'Packing...' -ForegroundColor Blue
 dotnet pack --graph --artifacts-path $Private:ArtifactsPath --configuration $Configuration --no-restore --no-build --include-symbols
+if ($LASTEXITCODE -ne 0) {
+  throw 'Pack failed.'
+}
--- scripts/Init-Project.ps1
+++ scripts/Init-Project.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = 'Stop'
+
 $Private:RootPath = Split-Path $PSScriptRoot
 $Private:MilkyIR = Join-Path $Private:RootPath 'MilkyIR.json'
 $Private:MilkyProps = Join-Path $Private:RootPath 'Milky.props'
@@ -8,18 +10,36 @@ $Private:ServerTarget = Join-Path $Private:RootPath 'src' 'Milky.Net.Server' 'Ge
 
 Write-Host 'Building Generator...' -ForegroundColor Blue
 dotnet build $Private:ModelGenerator
+if ($LASTEXITCODE -ne 0) {
+  throw 'Build Generator failed.'
+}
 
 Write-Host 'Downloading MilkyIR...' -ForegroundColor Blue
 dotnet run --no-build --no-launch-profile --project $Private:ModelGenerator -- download --output $Private:MilkyIR
+if ($LASTEXITCODE -ne 0) {
+  throw 'Download MilkyIR failed.'
+}
 
 Write-Host 'Generating MSBuild Props...' -ForegroundColor Blue
 dotnet run --no-build --no-launch-profile --project $Private:ModelGenerator -- generate props --source $Private:MilkyIR --output $Private:MilkyProps
+if ($LASTEXITCODE -ne 0) {
+  throw 'Generate MSBuild Props failed.'
+}
 
 Write-Host 'Generating Model Codes...' -ForegroundColor Blue
 dotnet run --no-build --no-launch-profile --project $Private:ModelGenerator -- generate models --source $Private:MilkyIR --output $Private:ModelTarget
+if ($LASTEXITCODE -ne 0) {
+  throw 'Generate Model Codes failed.'
+}
 
 Write-Host 'Generating Client Codes...' -ForegroundColor Blue
 dotnet run --no-build --no-launch-profile --project $Private:ModelGenerator -- generate client --source $Private:MilkyIR --output $Private:ClientTarget
+if ($LASTEXITCODE -ne 0) {
+  throw 'Generate Client Codes failed.'
+}
 
 Write-Host 'Generating Server Codes...' -ForegroundColor Blue
 dotnet run --no-build --no-launch-profile --project $Private:ModelGenerator -- generate server --source $Private:MilkyIR --output $Private:ServerTarget
+if ($LASTEXITCODE -ne 0) {
+  throw 'Generate Server Codes failed.'
+}
